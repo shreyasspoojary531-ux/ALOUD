a8b71ad8e444f378fd62428a3ebd6d7e56aff85d
SHREYAS <shreyasspoojary531@gmail.com>
2026-08-28 20:03:22 +0530

    TEST3

--- components/shaders/AppBackground.jsx
+++ components/shaders/AppBackground.jsx
@@ -37,7 +37,7 @@ export default function AppBackground() {
       <WarpFieldBackground
         speed={10}
         streakOpacity={0.20}
-        tileOpacity={0.10}
+        tileOpacity={0.05}
         fov={50}
         cameraZ={0}
         centerX={0}
